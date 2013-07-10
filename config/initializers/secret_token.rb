80c12f75dbcd63830bebc62beedd5bedbc4b90ac
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-10 15:47:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhineOneOne::Application.config.secret_token = '469460e03f9f07a20f2af1d931502d093c018daebedad60db9659538daf46844a312446e46dd7c72420204e951e236963ebf09760fd4e513f61e4b8bc88b2411'
