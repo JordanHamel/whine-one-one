542e97284256971bb14544e135b008273f72a429
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-10 17:44:19 -0700

    Index page

--- app/controllers/whine_controller.rb
+++ app/controllers/whine_controller.rb
@@ -10,4 +10,12 @@ class WhineController < ApplicationController
     Whine.create(:message => params[:message])
     redirect_to root_path
   end
+
+  def pick_whine
+    @whine = Whine.all.sample
+
+    respond_to do |format|
+      format.json { render json: @whine }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ WhineOneOne::Application.routes.draw do
   root to: 'whine#index'
 
   get 'whine', to: 'whine#new'
+  get 'pick_whine', to: 'whine#pick_whine'
   post 'submit', to: 'whine#create'
 end
