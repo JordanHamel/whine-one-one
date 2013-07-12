f1bea203177380e5d1e892d21c1a775ca197527c
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-12 16:51:03 -0700

    Max whine length = 200

--- app/models/whine.rb
+++ app/models/whine.rb
@@ -2,5 +2,5 @@ class Whine < ActiveRecord::Base
   attr_accessible :message
 
   validates_presence_of :message
-  validates_length_of :message, :maximum => 300
+  validates_length_of :message, :maximum => 200
 end
