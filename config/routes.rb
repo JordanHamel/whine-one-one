719a901ea6d4a08ee2bb8074dea98e8e11f4cc4b
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-16 13:13:43 -0700

    change /whine route to /whine/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 WhineOneOne::Application.routes.draw do
   root to: 'whine#index'
 
-  get 'whine', to: 'whine#new'
+  get '/whine/new', to: 'whine#new'
   get '/:tag/pick_whine', to: 'whine#pick_whine'
   get ':tag', to: 'tag#show'
   post 'submit', to: 'whine#create'
