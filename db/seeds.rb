8f11f91f2da927e57694fa4cb1d2d6b99b6a0aa1
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-16 18:38:11 -0700

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+messages = [
+  "The line for ice cream sandwiches is an hour long!",
+  "Bus is late again. When is this city ever gonna get it right?",
+  "I can\'t believe my bike got stolen! This town is supposed to have a LOW crime rate!",
+  "I really really really wish I could have a dog in my apartment. Why can\'t my landlord understand?",
+  "Fruit flies. Fruit flies everywhere.",
+  "I just vaccumed the carpet yesterday, and BOOM! Girl hair! I\'m pretty sure I\'m dating a dog...",
+  "My waterbottle leaked in my purse and now my phone is broken :(",
+  "A LEAFBLOWER WOKE ME UP THIS MORNING!!!",
+  "The kitchen is so dirty, I walk all the way around the house to avoid it.",
+  "My boyfriend just broke up with me.",
+  "My girlfiend just dumped my for my best friend",
+  "My toes always get cold in my snuggie. I wish they made snuggies with foot pockets.",
+  "My hands are too dry to turn the pages of this book. I also burnt my tounge with hot chocolate.",
+  "My dad lied to me. He said the ice cream was good and good for you. Now I\'m overweight.",
+]
+
+tags = [
+  "ridiculous",
+  "ice cream",
+  "bus",
+  "long wait",
+  "ridiculous",
+  "stolen",
+  "puppy deprived",
+  "apartment living",
+  "bugs",
+  "apartment living",
+  "hair",
+  "apartment living",
+  "ridiculous",
+  "wet phone",
+  "leafblower",
+  "apartment living",
+  "dirty dishes",
+  "apartment living",
+  "forever alone",
+  "broken heart",
+  "broken heart",
+  "bitch",
+  "snuggie",
+  "too cold",
+  "too hot",
+  "burnt tongue",
+  "liar",
+  "ridiculous",
+]
+
+tags.each do |t|
+  next if Tag.where(:text => t).length > 0
+  Tag.create(:text => t)
+end
+
+i = 0
+messages.each do |m|
+  if whine = Whine.create(:message => m)
+    tag1 = Tag.where(:text => tags[i]).first
+    tag2 = Tag.where(:text => tags[i+1]).first
+    WhineTag.create(:whine_id => whine.id, :tag_id => tag1.id)
+    WhineTag.create(:whine_id => whine.id, :tag_id => tag2.id)
+  end
+
+  i += 2
+end
