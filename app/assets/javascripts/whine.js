6f555dcd471a8b98e97476f15a005207b8f4f289
Jordan Hamel <jordan@nearbuysystems.com>
2013-07-23 13:25:43 -0700

    Increment counter after getting tags

--- app/assets/javascripts/whine.js
+++ app/assets/javascripts/whine.js
@@ -6,13 +6,13 @@ var tag;
 var nextWhine = function() {
   if (whines[i]) {
     $('#whine').append(whines[i].message);
-    i += 1;
     $.getJSON('/' + whines[i].id + '/get_tags.json', function(data) {
       for (var i=0; i<data.length; i++) {
         tag = data[i].text;
         $('#tags').append("<a href='/" + tag + "'> #" + tag + "</a> ");
       };
     });
+    i += 1;
   } else {
     $('#whine').append("It looks like you've hit the end :(");
   }
